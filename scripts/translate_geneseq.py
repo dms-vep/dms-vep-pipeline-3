bfcb88694f8321a5ad834827ab9697cc7d9c7c0d
jbloom <jbloom@fredhutch.org>
2023-07-20 13:32:09 -0700

    `translate_geneseq` allows genes not starting w start codon

--- scripts/translate_geneseq.py
+++ scripts/translate_geneseq.py
@@ -10,10 +10,13 @@ sys.stderr = sys.stdout = log = open(snakemake.log[0], "w")
 
 gene = Bio.SeqIO.read(snakemake.input.gene, "fasta").seq
 
-# first make sure gene can translate as CDS
-_ = gene.translate(cds=True)
-# now translate without cds flag to keep any stop codons
+# translate, making sure it gives valid protein with no stop except possibly at end
+if len(gene) % 3 != 0:
+    raise ValueError(f"{len(gene)} is not divisible by 3")
 protseq = gene.translate()
+assert len(protseq) == len(gene) // 3
+if "*" in protseq[:-1]:
+    raise ValueError(f"Premature stop codon in protein:\n{protseq}")
 
 with open(snakemake.output.prot, "w") as f:
     f.write(f">gene\n{str(protseq)}\n")

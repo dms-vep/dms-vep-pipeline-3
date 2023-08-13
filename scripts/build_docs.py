d8e0ac52e25c140bd9ca7b68f76e240cebdec386
jbloom <jbloom@fredhutch.org>
2023-08-13 15:38:00 -0700

    collapse lists in docs even if just 1 entry

--- scripts/build_docs.py
+++ scripts/build_docs.py
@@ -26,7 +26,7 @@ md_text = [
 subheading_depth = 1  # keys this deep are subheadings
 init_subheading = "##"  # first subheading level is this in markdown
 collapse_list = []  # nested list headings to collapse
-min_collapse_length = 2  # only collapse if at least this many entries
+min_collapse_length = 1  # only collapse if at least this many entries
 
 
 def process_docs(d, depth):

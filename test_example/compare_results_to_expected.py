a5e8dd6eff1ba13ac33560146910a95d0714a773
jbloom <jbloom@fredhutch.org>
2024-06-21 10:44:24 -0700

    relax thresholds for compare results to expected

--- test_example/compare_results_to_expected.py
+++ test_example/compare_results_to_expected.py
@@ -27,6 +27,6 @@ for f in to_compare:
 
     print("Comparing actual and expected results...")
     pd.testing.assert_frame_equal(
-        actual, expected, check_exact=False, atol=5e-2, rtol=1e-1
+        actual, expected, check_exact=False, atol=0.1, rtol=0.2
     )
     print("Actual and expected results are sufficiently similar.")
